0860a5a4d7de9c4c44fd1b010bbf46c8d1df586c
DaniilKononov <danil.kononov.05@mail.ru>
2024-10-26 09:33:03 +0700

    Лабораторная работа №3

---
+++ lab3/js/lab3_1_10.js
@@ -0,0 +1,25 @@
+function compareNumbers() {
+	let first_number = prompt('Введите первое целое число: ');							//ввод первого числа
+	let second_number = prompt('Введите второе целое число: ');							//ввод второго числа
+	let statement = 'Ровно одно из чисел нечетное'
+	
+	first_number = Number(first_number);												//преобразованиев строки в число
+	second_number = Number(second_number);												//преобразованиев строки в число
+	
+	if (Number.isInteger(first_number) && Number.isInteger(second_number)) {			//проверка, является ли число целым
+		if ((Math.abs(first_number)%2 == 1) || (Math.abs(second_number)%2 == 1)) {		//проверка, есть ли среди чисел нечетное
+			if ((Math.abs(first_number)%2 == 1) && (Math.abs(second_number)%2 == 1)) {	//проверка, оба ли числа нечетные
+				alert(statement + '\n' + 'FALSE');										//вывод FALSE, если оба нечетные
+			} else {
+				alert(statement + '\n' + 'TRUE');										//вывод TRUE, если только одно нечетное
+			} 
+		} else {
+			alert(statement + '\n' + 'FALSE');											//вывод FALSE, если оба числа четные
+		}
+	} else {																			//если число не целое
+		alert('Введено не целое число. Попробуйте снова');								//или вовсе не число, то происходит
+		compareNumbers();																//вызов этой же функции
+	}
+}
+
+compareNumbers();																		//вызов функции
---
+++ lab3/js/lab3_2_10.js
@@ -0,0 +1,21 @@
+function compareNumbers() {
+	let first_number = prompt('Введите первое целое число: ');						//ввод первого числа
+	let second_number = prompt('Введите второе целое число: ');						//ввод второго числа
+	
+	first_number = Number(first_number);											//преобразованиев строки в число
+	second_number = Number(second_number);											//преобразованиев строки в число
+	
+	if (Number.isInteger(first_number) && Number.isInteger(second_number)) {		//проверка, является ли число целым
+		if (first_number == second_number) {										//если числа равны
+			second_number = first_number = 0;										//присваивание обоим числам нулевого значения
+		} else {
+			second_number = first_number = first_number + second_number;			//присваивание обоим числам суммы этих чисел
+		}
+		alert('Число A = ' + first_number + '\n' + 'Число B = ' + second_number);	//вывод чисел
+	} else {																		//если число не целое
+		alert('Введено не целое число. Попробуйте снова');							//или вовсе не число, то происходит
+		compareNumbers();															//вызов этой же функции
+	}
+}
+
+compareNumbers();																	//вызов функции
